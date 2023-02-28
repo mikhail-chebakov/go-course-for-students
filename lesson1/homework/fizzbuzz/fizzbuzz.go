802773fa5541eeecfb8f8f88c258ab590947e244
mikhail <m.chebakov@tinkoff.ru>
2023-02-28 14:28:56 +0300

    solution

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,12 @@
 package fizzbuzz
 
 func FizzBuzz(i int) string {
-	// TODO
+	if i % 15 == 0 {
+		return "FizzBuzz"
+	} else if i % 5 == 0 {
+		return "Buzz"
+	} else if i % 3 == 0 {
+		return "Fizz"
+	}
 	return ""
 }
